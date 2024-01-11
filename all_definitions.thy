ef38a699728075c8fa656d98c450829b7c8e9652
Matthias Sleurink <m.sleurink@student.utwente.nl>
2024-01-11 14:34:34 +0100

    all definitions file

---
+++ all_definitions.thy
@@ -0,0 +1,15 @@
+theory all_definitions
+  imports basic_definitions
+          \<comment> \<open>Add all derived definitions here\<close>
+begin
+
+text \<open>
+This is the second collation phase of the dependency graph.
+Importing this files give you all the basic and the derived definitions.
+The intention is for this file to be the entrypoint.
+So the examples will also import this file to start.
+The dependency graph should contain as much parallelism as attainable using the
+ "multiple diamond" structure as explained in the basic definitions file.
+\<close>
+
+end
