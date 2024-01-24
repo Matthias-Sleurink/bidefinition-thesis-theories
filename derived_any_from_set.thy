ad39500c98d5b27cd6a1347f3718806c7e705a37
Matthias Sleurink <m.sleurink@student.utwente.nl>
2024-01-24 15:36:21 +0100

    rewrite from set with char for predicate

--- derived_any_from_set.thy
+++ derived_any_from_set.thy
@@ -1,6 +1,6 @@
 theory derived_any_from_set
   imports basic_definitions
-          derived_dep_then
+          derived_char_for_predicate
 begin
 
 text \<open>
@@ -8,7 +8,7 @@ Parse any character in this set
 \<close>
 
 definition any_from_set :: "char set \<Rightarrow> char bidef" where
-  "any_from_set s = dep_then one_char (\<lambda>found. if found\<in>s then return found else fail) id"
+  "any_from_set s = char_for_predicate (\<lambda>found. found\<in>s)"
 
 
 
@@ -21,14 +21,12 @@ lemma any_from_set_is_nonterm[NER_simps]:
 lemma any_from_set_is_error[NER_simps]:
   "is_error (parse (any_from_set s)) i \<longleftrightarrow> i = [] \<or> hd i \<notin> s"
   unfolding any_from_set_def
-  apply (auto simp add: NER_simps)
-  by (meson list.exhaust_sel)
+  by (simp add: NER_simps)
 
 lemma any_from_set_has_result[NER_simps]:
   "has_result (parse (any_from_set s)) i r l \<longleftrightarrow> i \<noteq> [] \<and> hd i \<in> s \<and> i = r#l"
   unfolding any_from_set_def
-  apply (clarsimp simp add: NER_simps)
-  by force
+  by (simp add: NER_simps)
 
 
 
@@ -36,7 +34,7 @@ lemma any_from_set_has_result[NER_simps]:
 lemma any_from_set_p_is_error[fp_NER]:
   "p_is_error (print (any_from_set s)) i \<longleftrightarrow> i \<notin> s"
   unfolding any_from_set_def
-  by (auto simp add: fp_NER)
+  by (simp add: fp_NER)
 
 lemma any_from_set_p_has_result[fp_NER]:
   "p_has_result (print (any_from_set s)) i pr \<longleftrightarrow> i\<in>s \<and> pr = [i]"
@@ -48,10 +46,7 @@ lemma any_from_set_p_has_result[fp_NER]:
 \<comment> \<open>Well formed\<close>
 lemma any_from_set_well_formed[bi_well_formed_simps]:
   "bidef_well_formed (any_from_set s)"
-  apply wf_init
-  subgoal by (auto simp add: parser_can_parse_print_result_def fp_NER NER_simps)
-  subgoal by (auto simp add: printer_can_print_parse_result_def fp_NER NER_simps)
-  done
+  by (simp add: any_from_set_def char_for_predicate_well_formed)
 
 
 end
