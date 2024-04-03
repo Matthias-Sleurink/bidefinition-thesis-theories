dfc4f98e9e9e357df047dc230b334b6878c147ce
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-04-03 17:05:54 +0200

    unfinished proof for well formed seperatedBy
    This version has a ton of extra assms that may be provable from other assms.
    That is an outstanding TODO.
    If we fill this in with the bidefs for the number parser I'm sure we could prove it without this easier.

--- derived_seperatedby.thy
+++ derived_seperatedby.thy
@@ -127,7 +127,71 @@ lemma snd_comp_pair_id[simp]:
   by fastforce
 
 \<comment> \<open>Well formed\<close>
-
-
-
+lemma seperatedBy_well_formed:
+  assumes "good_seperatedBy_oracle sep sep_oracle"
+  assumes "pa_does_not_eat_into_pb_nondep elem sep"
+  assumes "pa_does_not_eat_into_pb_nondep sep elem"
+  assumes "bidef_well_formed elem"
+  assumes "bidef_well_formed sep"
+  assumes "is_error (parse elem) []"
+  assumes "pa_does_not_eat_into_pb_nondep elem (many (b_then sep elem))"
+  assumes "parser_can_parse_print_result (parse (many (b_then sep elem))) (print (many (b_then sep elem)))" \<comment> \<open>Would be ideal to get this from WF elem, sep, and more.\<close>
+  shows "bidef_well_formed (seperatedBy sep elem sep_oracle)"
+  unfolding seperatedBy_def
+  apply (rule transform_well_formed3)
+  defer
+  subgoal
+    using assms(1,4,5,6)
+    unfolding well_formed_transform_funcs3_def good_seperatedBy_oracle_def
+              bidef_well_formed_def printer_can_print_parse_result_def
+              seperatedByBase_def
+    apply (auto simp add: fp_NER NER_simps split: option.splits)
+    subgoal for x i a b xa l'
+      unfolding optional_p_has_result(2) b_then_p_has_result
+      apply (auto simp add: fp_NER NER_simps split: option.splits)
+      apply (induction b arbitrary: x i a xa l')
+      subgoal by (auto simp add: fp_NER)
+      subgoal
+        apply (auto simp add: fp_NER NER_simps split: option.splits)
+        unfolding many_p_has_result_safe \<comment> \<open>Inside the Ex auto does not do these, so we force it\<close>
+        by (metis b_then_p_has_result(1))
+      done
+    subgoal for _ t by (cases t; clarsimp; blast)
+    subgoal for x t a b ta tb
+      apply (cases t; clarsimp) \<comment> \<open>t = a # as\<close>
+      subgoal for as
+        using assms(7)[unfolded pa_does_not_eat_into_pb_nondep_def,
+                       rule_format,
+                       of a ta \<open>(map (Pair sep_oracle) as)\<close> tb]
+        apply clarsimp
+        using assms(8)[unfolded parser_can_parse_print_result_def,
+                       rule_format,
+                       of \<open>(map (Pair sep_oracle) as)\<close> tb]
+        apply clarsimp
+        apply (rule exI[of _ \<open>[]\<close>])
+        apply (rule exI[of _ \<open>Some (a, (map (Pair sep_oracle) as))\<close>])
+        by auto
+      done
+    done
+  apply (rule optional_well_formed)
+  subgoal by (clarsimp simp add: seperatedByBase_def NER_simps assms(6))
+  unfolding seperatedByBase_def
+  apply (rule b_then_well_formed)
+  subgoal by (rule assms(4))
+    defer
+  subgoal by (rule assms(7))
+  apply (rule well_formed_does_not_grow_by_printer)
+    defer defer
+  subgoal
+    apply (clarsimp simp add: NER_simps assms(6))
+    by (metis assms(8) b_then_is_error many_has_result_safe(1) many_p_has_result_safe(1) parser_can_parse_print_result_simp)
+  subgoal 
+    unfolding parse_result_cannot_be_grown_by_printer_def
+    apply (clarsimp simp add: NER_simps assms(6))
+    sorry \<comment> \<open>SORRY HERE, TODO!\<close>
+  apply (rule b_then_well_formed)
+  subgoal by (rule assms(5))
+  subgoal by (rule assms(4))
+  subgoal by (rule assms(3))
+  oops
 end
