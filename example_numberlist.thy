1d1207be96e49a58c52c0d12cb722cae769343e1
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-04-05 12:25:51 +0200

    finalise numberlist well formed

--- example_numberlist.thy
+++ example_numberlist.thy
@@ -122,23 +122,21 @@ lemma can_parse_print_result_then_many1_ws_nat:
     subgoal for ws n i' i'_pr tl_ws
       using p_has_result_many_whitespace_implies_i_eq_r[of \<open>tl ws\<close> tl_ws]
       apply clarsimp
-      \<comment> \<open>l'a = print_nat n @ i'_pr\<close>
-      \<comment> \<open>l' = dropWhile (\<lambda>x. x \<in> derived_digit_char.digit_chars) l'a\<close>
-      \<comment> \<open>so, l' = dropWhile (\<lambda>x. x \<in> derived_digit_char.digit_chars) (print_nat n @ i'_pr)\<close>
-      \<comment> \<open>Where, technically we can remove the print_nat n there, but it's probably better to
-          introduce that knowledge into the proof state.\<close>
       apply (rule exI[of _ \<open>dropWhile (\<lambda>x. x \<in> derived_digit_char.digit_chars) (print_nat n @ i'_pr)\<close>])
       apply (rule conjI)
       subgoal
         apply (rule exI[of _ \<open>print_nat n @ i'_pr\<close>])
         apply auto
         subgoal
-          \<comment> \<open>TODO\<close>
-          sorry
+          unfolding whitespace_char_def any_from_set_def
+          apply (subst many_char_for_predicate_has_result)
+          using many_char_for_predicate_p_has_result3[of \<open>\<lambda>found. found \<in> whitespace_chars\<close> \<open>tl ws\<close> \<open>tl ws\<close>]
+                takeWhile_eq_Nil_iff[of \<open>\<lambda>found. found \<in> whitespace_chars\<close> \<open>print_nat n @ i'_pr\<close>]
+                no_space_hd_nat dropWhile_hd_no_match[of \<open>\<lambda>found. found \<in> whitespace_chars\<close> \<open>print_nat n @ i'_pr\<close>]
+          by force
         subgoal
-          using print_nat_never_empty print_nat_domain
-          \<comment> \<open>TODO, this can be done nice very easily\<close>
-          by (metis append_is_Nil_conv digit_chars_def hd_append2 print_nat_hd takeWhile_eq_Nil_iff)
+          using takeWhile_eq_Nil_iff[of \<open>\<lambda>x. x \<in> derived_digit_char.digit_chars\<close> \<open>print_nat n @ i'_pr\<close>]
+          by clarsimp
         subgoal
           using aux1[of i' i'_pr]
           apply clarsimp
@@ -160,9 +158,33 @@ lemma can_parse_print_result_then_many1_ws_nat:
         by (metis digit_chars_def dropWhile.simps(1) dropWhile_hd_no_match)
       done
     done
-  oops
+  done
 
+lemma many_ws_has_result:
+  "has_result (parse (many whitespace_char)) i r l \<longleftrightarrow> r = takeWhile (\<lambda>c. c\<in>whitespace_chars) i \<and> l = dropWhile (\<lambda>c. c\<in>whitespace_chars) i"
+  unfolding whitespace_char_def any_from_set_def
+  by (rule many_char_for_predicate_has_result)
 
+lemma many1_ws_natb_cannot_be_grown_by_many_self:
+  "parse_result_cannot_be_grown_by_printer
+     (parse (b_then (many1 whitespace_char) nat_b))
+     (print (many (b_then (many1 whitespace_char) nat_b)))"
+  unfolding parse_result_cannot_be_grown_by_printer_def
+  apply (auto simp add: fp_NER NER_simps)
+  subgoal for ws_i ws_r pri prt ws_l
+    apply (rule exI[of _ \<open>dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i @ prt)\<close>])
+    apply (auto simp add: many_ws_has_result)
+    subgoal by (simp add: takeWhile_eq_Nil_iff)
+    subgoal
+      by (metis append.right_neutral aux1 digit_chars_def list.collapse takeWhile_dropWhile_id takeWhile_tail)
+    subgoal
+      by (metis \<open>\<And>x. \<lbrakk>ws_r \<noteq> []; ws_i \<noteq> []; hd ws_r = hd ws_i; hd ws_i \<in> whitespace_chars; p_has_result (print (many (b_then (many1 whitespace_char) nat_b))) pri prt; takeWhile (\<lambda>x. x \<in> derived_digit_char.digit_chars) (dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i)) \<noteq> []; x \<in> set (tl ws_i); x \<notin> whitespace_chars; tl ws_r = takeWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i); ws_l = dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i)\<rbrakk> \<Longrightarrow> takeWhile (\<lambda>x. x \<in> derived_digit_char.digit_chars) (dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i) @ prt) @ dropWhile (\<lambda>x. x \<in> derived_digit_char.digit_chars) (dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i)) = dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i)\<close>
+                append_same_eq takeWhile_dropWhile_id)
+    subgoal
+      by (metis \<open>\<And>x. \<lbrakk>ws_r \<noteq> []; ws_i \<noteq> []; hd ws_r = hd ws_i; hd ws_i \<in> whitespace_chars; p_has_result (print (many (b_then (many1 whitespace_char) nat_b))) pri prt; takeWhile (\<lambda>x. x \<in> derived_digit_char.digit_chars) (dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i)) \<noteq> []; x \<in> set (tl ws_i); x \<notin> whitespace_chars; tl ws_r = takeWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i); ws_l = dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i)\<rbrakk> \<Longrightarrow> takeWhile (\<lambda>x. x \<in> derived_digit_char.digit_chars) (dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i) @ prt) @ dropWhile (\<lambda>x. x \<in> derived_digit_char.digit_chars) (dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i)) = dropWhile (\<lambda>c. c \<in> whitespace_chars) (tl ws_i)\<close>
+                append_Nil2 dropWhile_append1 same_append_eq self_append_conv2 takeWhile_dropWhile_id takeWhile_eq_all_conv)
+    done
+  done
 
 
 lemma numberlist_well_formed:
@@ -173,14 +195,12 @@ lemma numberlist_well_formed:
   subgoal by (rule nat_does_not_eat_into_many1_whitespace)
   subgoal by (rule many1_whitespace_does_not_eat_into_nat)
   subgoal by (rule nat_b_well_formed)
-      defer
+  subgoal by (clarsimp simp add: many1_char_for_predicate_well_formed whitespace_char_def any_from_set_def)
   subgoal by (clarsimp simp add: NER_simps)
   subgoal by (rule nat_does_not_into_ws_then_nat)
-  defer defer
-  subgoal by (clarsimp simp add: many1_char_for_predicate_well_formed whitespace_char_def any_from_set_def)
-  
-  
-  oops
+  subgoal by (rule can_parse_print_result_then_many1_ws_nat)
+  subgoal by (rule many1_ws_natb_cannot_be_grown_by_many_self)
+  done
 
 
 end
