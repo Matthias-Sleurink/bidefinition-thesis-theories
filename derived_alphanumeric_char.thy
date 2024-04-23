a1bb530f6f4e6cfebc4ece64c0c52b32236b03ed
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-04-23 17:09:36 +0200

    Does not peek past end alphanumeric_char

--- derived_alphanumeric_char.thy
+++ derived_alphanumeric_char.thy
@@ -76,6 +76,14 @@ lemma alphanumeric_char_PASI:
 
 
 
+\<comment> \<open>Does not peek past end\<close>
+lemma alphanumeric_char_does_not_peek_past_end[peek_past_end_simps]:
+  "does_not_peek_past_end (parse alphanumeric_char)"
+  unfolding alphanumeric_char_def
+  by (clarsimp simp add: peek_past_end_simps)
+
+
+
 \<comment> \<open>Well Formed\<close>
 lemma alphanumeric_char_well_formed:
   "bidef_well_formed alphanumeric_char"
