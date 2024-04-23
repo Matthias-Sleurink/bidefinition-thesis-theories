92ba46de486991658755944304f437353dcf0364
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-04-23 15:22:19 +0200

    Does not peek past end whitespace_char

--- derived_whitespace_char.thy
+++ derived_whitespace_char.thy
@@ -73,6 +73,14 @@ lemma whitespace_char_PASI:
 
 
 
+\<comment> \<open>Does not peek past end\<close>
+lemma whitespace_char_does_not_peek_past_end[peek_past_end_simps]:
+  "does_not_peek_past_end (parse whitespace_char)"
+  unfolding whitespace_char_def
+  by (clarsimp simp add: peek_past_end_simps)
+
+
+
 \<comment> \<open>Well Formed\<close>
 lemma whitespace_char_well_formed:
   "bidef_well_formed whitespace_char"
