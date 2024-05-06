26879b4268ef1b1afeb5fd1e667ac7356b2d75d9
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-05-06 13:52:58 +0200

    FPCI for char for predicate fix that simp rule was wrong way round

--- derived_char_for_predicate.thy
+++ derived_char_for_predicate.thy
@@ -76,7 +76,7 @@ lemma char_for_predicate_does_not_peek_past_end[peek_past_end_simps]:
 
 \<comment> \<open>First printed char\<close>
 lemma char_for_predicate_fpci:
-  shows "(P i \<and> c = i) \<longleftrightarrow> first_printed_chari (print (char_for_predicate P)) i c"
+  shows "first_printed_chari (print (char_for_predicate P)) i c \<longleftrightarrow> (P i \<and> c = i)"
   unfolding first_printed_chari_def
   by (clarsimp simp add: char_for_predicate_p_has_result; blast)
 
