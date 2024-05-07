e11347214bbd840f9232bbe2e2ea12b3aac71037
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-05-07 16:37:00 +0200

    print_empty for uppercase char

--- derived_uppercase_char.thy
+++ derived_uppercase_char.thy
@@ -51,6 +51,10 @@ lemma uppercase_char_p_has_result[fp_NER]:
   "p_has_result (print uppercase_char) i s \<longleftrightarrow> i \<in> uppercase_chars \<and> s = [i]"
   by (simp add: uppercase_char_def any_from_set_p_has_result)
 
+lemma uppercase_char_print_empty[print_empty, fp_NER]:
+  "p_has_result (print uppercase_char) i [] \<longleftrightarrow> False"
+  by (clarsimp simp add: uppercase_char_def print_empty)
+
 
 
 \<comment> \<open>PNGI, PASI\<close>
