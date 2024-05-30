3ec8c5fc9ae8dc645a2ccb3daefa267b409cf053
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-05-30 16:34:27 +0200

    The start of WF expression and expression does not eat into ), both showing that current architecture is not enough for it.

--- example_expression_parser.thy
+++ example_expression_parser.thy
@@ -319,131 +319,74 @@ lemma "p_has_result (print Expression) (Additive [Multiply [Parenthesised (Addit
 
 
 section \<open>Well formed\<close>
+lemma Expression_no_eat_into_paren:
+  "pa_does_not_eat_into_pb_nondep Expression (ws_char_ws CHR '')'')"
+  unfolding pa_does_not_eat_into_pb_nondep_def
+  apply (auto simp add: NER_simps fp_NER)
+  subgoal for r t
+    apply (cases r; clarsimp)
+    subgoal for xs
+      apply (induction xs arbitrary: t rule: rev_induct)
+      subgoal by (subst (asm) Expression_def; auto simp add: fp_NER AddE_def)
+      subgoal for m ms t''
+        apply (subst Expression_def)
+        apply (subst (asm) (3) Expression_def)
+        apply (subst (asm) (1) Expression_def)
+        apply (subst (asm) (2) Expression_def)
+        apply (auto simp add: fp_NER NER_simps AddE_def)
+        
+        
+        sorry
+      done
+    \<comment> \<open>All other Ex's cannot be printed by Expression so the assm is false:\<close>
+    subgoal by (subst (asm) Expression_def; clarsimp simp add: fp_NER)
+    subgoal by (subst (asm) Expression_def; clarsimp simp add: fp_NER)
+    subgoal by (subst (asm) Expression_def; clarsimp simp add: fp_NER)
+    done
+  oops
+      
+
+
+
+\<comment> \<open>This is not a feasible strategy, as you can see from the note below.\<close>
 lemma expression_well_formed:
   "bidef_well_formed Expression"
   apply (subst Expression_def)
-  
-  oops
-
-
-
-\<comment> \<open>NER\<close>
-\<comment> \<open>This needs to be done not for expression but for expressionR\<close>
-lemma expression_is_nonterm[NER_simps]:
-  "is_nonterm (parse Expression) i \<longleftrightarrow> False"
-  oops
-
-lemma fail_is_error[NER_simps]:
-  "is_error (parse fail) i \<longleftrightarrow> True"
-  "is_error fail_p       i \<longleftrightarrow> True"
-  by (simp add: fail_def is_error_def)+
-
-lemma fail_has_result[NER_simps]:
-  "has_result (parse fail) i r l \<longleftrightarrow> False"
-  "has_result fail_p       i r l \<longleftrightarrow> False"
-  by (simp add: fail_def has_result_def)+
-
-lemma fail_has_result_c[NER_simps]:
-  "has_result_c (parse fail) c r l \<longleftrightarrow> False"
-  "has_result_c fail_p       c r l \<longleftrightarrow> False"
-  by (simp add: has_result_c_def fail_has_result)+
-
-lemma fail_has_result_ci[NER_simps]:
-  "has_result_ci (parse fail) i c r l \<longleftrightarrow> False"
-  "has_result_ci fail_p       i c r l \<longleftrightarrow> False"
-  by (simp add: has_result_ci_def fail_has_result_c)+
-
-
-
-\<comment> \<open>FP NER\<close>
-lemma fail_p_is_nonterm[fp_NER]:
-  "p_is_nonterm (print fail) i \<longleftrightarrow> False"
-  "p_is_nonterm fail_pr i \<longleftrightarrow> False"
-  by (simp add: fail_def p_is_nonterm_def)+
-
-lemma fail_p_is_error[fp_NER]:
-  "p_is_error (print fail) i \<longleftrightarrow> True"
-  "p_is_error fail_pr      i \<longleftrightarrow> True"
-  by (simp add: fail_def p_is_error_def)+
-
-lemma fail_p_has_result[fp_NER]:
-  "p_has_result (print fail) i r \<longleftrightarrow> False"
-  "p_has_result fail_pr      i r \<longleftrightarrow> False"
-  by (simp add: fail_def p_has_result_def)+
-
-lemma fail_print_empty[print_empty, fp_NER]:
-  "p_has_result (print fail) i [] \<longleftrightarrow> False"
-  by (rule fail_p_has_result(1))
-
-
-
-\<comment> \<open>PNGI, PASI\<close>
-lemma fail_PNGI:
-  "PNGI (parse fail)"
-  by (simp add: PNGI_def NER_simps)
-
-lemma fail_PASI:
-  "PASI (parse fail)"
-  by (simp add: PASI_def NER_simps)
-
-
-
-\<comment> \<open>Charset\<close>
-lemma charset_fail:
-  "charset (parse fail) = {}"
-  unfolding charset_def
-  by (clarsimp simp add: NER_simps)
-
-lemma first_chars_fail:
-  "first_chars (print fail) = {}"
-  unfolding first_chars_def
-  by (clarsimp simp add: fp_NER)
-
-
-
-\<comment> \<open>Does not peek past end\<close>
-lemma fail_does_not_peek_past_end[peek_past_end_simps]:
-  shows "does_not_peek_past_end (parse fail)"
-  unfolding does_not_peek_past_end_def
-  by (clarsimp simp add: fail_has_result)
-
-
-\<comment> \<open>Does not consume past char.\<close>
-lemma fail_does_not_consume_past_char:
-  shows "does_not_consume_past_char (parse fail) ch"
-  unfolding does_not_consume_past_char_def
-  by (clarsimp simp add: fail_has_result)
-
-lemma fail_does_not_consume_past_char2:
-  shows "does_not_consume_past_char2 (parse fail) ch"
-  unfolding does_not_consume_past_char2_def
-  by (clarsimp simp add: fail_has_result)
-
-
-\<comment> \<open>First printed char\<close>
-lemma fail_fpci[fpci_simps]:
-  shows "\<nexists>i c. first_printed_chari (print fail) i c"
-        "first_printed_chari (print fail) i c \<longleftrightarrow> False"
-  unfolding first_printed_chari_def
-  by (clarsimp simp add: fail_p_has_result)+
-
-lemma fail_fpc[fpc_simps]:
-  shows "\<nexists>i c. fpc (parse fail) i c"
-        "fpc (parse fail) i c \<longleftrightarrow> False"
-  unfolding fpc_def
-  by (clarsimp simp add: fail_has_result)+
-
-
-
-\<comment> \<open>Well Formed\<close>
-lemma fail_well_formed:
-  "bidef_well_formed fail"
-  apply wf_init
-  subgoal by (rule fail_PNGI)
-  subgoal by (simp add: parser_can_parse_print_result_def fp_NER)
-  subgoal by (simp add: printer_can_print_parse_result_def NER_simps)
-  done
-
-
+  apply (rule ftransform_well_formed)
+  subgoal sorry
+  subgoal sorry
+  subgoal
+    unfolding AddE_def
+    apply (rule transform_well_formed)
+    subgoal
+      apply (rule separated_by1_well_formed)
+      subgoal by (clarsimp simp add: ws_char_ws_p_has_result)
+      subgoal
+        unfolding MultE_def
+        apply (rule transform_well_formed)
+        subgoal
+          apply (rule separated_by1_well_formed)
+          subgoal by (clarsimp simp add: ws_char_ws_p_has_result)
+          subgoal
+            unfolding NOE_def
+            apply (rule ftransform_well_formed)
+            subgoal sorry
+            subgoal sorry
+            subgoal
+              apply (rule or_well_formed)
+              subgoal by (rule Number_well_formed)
+              subgoal
+                unfolding ws_parenthesised_def
+                apply (rule transform_well_formed)
+                subgoal
+                  apply (rule b_then_well_formed)
+                  subgoal by (rule ws_char_ws_well_formed; clarsimp)
+                  subgoal
+                    apply (rule b_then_well_formed)
+                    subgoal \<comment> \<open>NOTE: This is the same as the original goal!\<close> sorry
+                    subgoal by (rule ws_char_ws_well_formed; clarsimp)
+                    subgoal sorry
+                    done
+                  oops
 
 end
