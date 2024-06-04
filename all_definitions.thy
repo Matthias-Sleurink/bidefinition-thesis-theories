4bff958c2d5737410693ac505a073ac89a2b3179
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-06-04 17:21:39 +0200

    add int to all definitions

--- all_definitions.thy
+++ all_definitions.thy
@@ -27,6 +27,7 @@ theory all_definitions
           derived_ws_comma_ws
           derived_ws_char_ws
           derived_separated_by1
+          derived_int
           \<comment> \<open>Add all derived definitions here\<close>
 begin
 
