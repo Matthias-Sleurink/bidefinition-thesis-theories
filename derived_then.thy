531e4cc93f0091c5c75370527dc2ddbd781986d8
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-06-25 17:12:35 +0200

    Notes on improving then_does_not_consume_past3

--- derived_then.thy
+++ derived_then.thy
@@ -506,7 +506,7 @@ lemma then_does_not_consume_past3:
           \<comment> \<open>\<^term>\<open>does_not_peek_past_end (parse A)\<close> would solve it.\<close>
           using dncpc_B_c[unfolded does_not_consume_past_char3_def]
           apply (cases c'')
-          subgoal using no_empty_res_B by blast
+          subgoal using no_empty_res_B by blast \<comment> \<open>This can also be proven in a few other ways.\<close>
           subgoal using fpc_B_dncpc_A[unfolded does_not_consume_past_char3_def fpc_def] by fastforce
           done
         subgoal using dncpc_B_c[unfolded does_not_consume_past_char3_def] by fastforce
