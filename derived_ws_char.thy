97fa6ff18edcc60c35b2e022f9bef7147334649b
Sleurink, M. (Matthias, Student M-CS) <m.sleurink@student.utwente.nl>
2024-07-01 17:39:24 +0200

    ws char cannot have a result where i = l

--- derived_ws_char.thy
+++ derived_ws_char.thy
@@ -34,6 +34,10 @@ lemma ws_char_has_result[NER_simps]:
       \<and> hd (dropWhile (\<lambda>x. x \<in>whitespace_chars) i) = c"
   apply (auto simp add: ws_char_def NER_simps whitespace_char_def any_from_set_def)
   by (metis dropWhile_eq_Nil_conv hd_Cons_tl)
+lemma ws_char_no_result_same_leftover[NER_simps]:
+  "has_result (parse (ws_char c)) i r i \<longleftrightarrow> False"
+  apply (clarsimp simp add: NER_simps)
+  by (metis hd_Cons_tl impossible_Cons length_dropWhile_le length_greater_0_conv length_pos_if_in_set list.sel(2))
 
 lemma ws_char_has_result_c[NER_simps]:
   "has_result_c (parse (ws_char c)) i r l \<longleftrightarrow>
